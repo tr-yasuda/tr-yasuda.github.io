5af3d79edafe4ad86764dd9e24461ab8e4d7f070
tr-yasuda <yoshitada.yasuda@ntt.com>
2026-01-03 21:35:53 +0900

    feat(theme): add theme provider and configure Tailwind CSS integration

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,8 +1,10 @@
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+import { ModeToggle } from '@/components/mode-toggle.tsx';
+import { ThemeProvider } from '@/components/theme-provider.tsx';
 
 const RootLayout = () => (
-  <>
+  <ThemeProvider defaultTheme="system" storageKey="theme">
     <div className="p-2 flex gap-2">
       <Link to="/" className="[&.active]:font-bold">
         Home
@@ -10,11 +12,12 @@ const RootLayout = () => (
       <Link to="/about" className="[&.active]:font-bold">
         About
       </Link>
+      <ModeToggle />
     </div>
     <hr />
     <Outlet />
     <TanStackRouterDevtools />
-  </>
+  </ThemeProvider>
 );
 
 export const Route = createRootRoute({ component: RootLayout });
